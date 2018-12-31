70a4a61db7c6536aab46c5f0afa067da7187239d
Ed Mamerto <edmamerto@gmail.com>
2018-12-31 11:05:40 -0500

    init

---
+++ start/src/TodoContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import TodoContainer from './TodoContainer'
+
+class TodoConatiner extends Component {
+	render() {
+		return (
+			<Note />
+		)
+	}
+}
+
+export default TodoConatiner
