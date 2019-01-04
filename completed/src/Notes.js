d79e8732a2f08974cb564e6df9cc203b627e318d
Ed Mamerto <edmamerto@gmail.com>
2019-01-03 23:17:35 -0500

    add react bootstrap

---
+++ completed/src/Notes.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
+
+class Notes extends Component {
+
+	render() {
+		return (
+			<Button bsStyle="warning">
+				Hello World
+			</Button>
+		)
+	}
+}
+
+export default Notes
---
+++ completed/src/TodoContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Notes from './Notes'
+
+class TodoConatiner extends Component {
+	render() {
+		return (
+			<Notes />
+		)
+	}
+}
+
+export default TodoConatiner
