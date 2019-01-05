18dcf721c4a89ad615949c17cc877824ece9402d
Ed Mamerto <edmamerto@gmail.com>
2019-01-04 23:31:58 -0500

    add textarea to panel

--- completed/src/Notes.js
+++ completed/src/Notes.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
-import { Panel, Grid, Row, Col, Button } from 'react-bootstrap'
+import { 
+	Panel, 
+	Grid, 
+	Row, 
+	Col, 
+	Button, 
+	FormGroup,
+	FormControl
+} from 'react-bootstrap'
 
 class Notes extends Component {
 
@@ -13,7 +21,11 @@ class Notes extends Component {
 					<Row>
 						<Col xs={6} md={4}>
 							<Panel>
-								<Panel.Body>Basic panel example</Panel.Body>
+								<Panel.Body>
+								    <FormGroup controlId="formControlsTextarea">
+								      <FormControl componentClass="textarea" placeholder="textarea" />
+								    </FormGroup>
+								</Panel.Body>
 								<Panel.Footer>
 									<Button bsStyle="link" bsSize="xsmall">edit</Button>
 									<Button bsStyle="primary" bsSize="xsmall">done</Button>
