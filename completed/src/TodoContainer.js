f31e42a8667a08db20c31d60bf36c6b4458dc7a4
Ed Mamerto <edmamerto@gmail.com>
2019-01-05 09:40:11 -0500

    add notes state and pass as props

--- completed/src/TodoContainer.js
+++ completed/src/TodoContainer.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react'
 import Notes from './Notes'
 
 class TodoConatiner extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			notes: [
+				{
+					id: 33,
+					note: "Call Lisa"
+				},
+				{
+					id: 34,
+					note: "Email John"
+				},
+				{
+					id: 35,
+					note: "Order printer ink"
+				}
+			]
+		}
+	}
+
 	render() {
 		return (
-			<Notes />
+			<Notes notes={this.state.notes}/>
 		)
 	}
 }
