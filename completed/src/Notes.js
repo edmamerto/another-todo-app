c06215c45a45a74381d0304a2772318d3aed11bf
Ed Mamerto <edmamerto@gmail.com>
2019-01-04 23:14:19 -0500

    mark up todo header and card

--- completed/src/Notes.js
+++ completed/src/Notes.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
-import { Button } from 'react-bootstrap'
+import { Panel, Grid, Row, Col } from 'react-bootstrap'
 
 class Notes extends Component {
 
 	render() {
 		return (
-			<Button bsStyle="warning">
-				Hello World
-			</Button>
+			<div>
+				<Grid>
+					<h1>To Do</h1>
+				</Grid>
+				<Grid>
+					<Row>
+						<Col xs={6} md={4}>
+							<Panel>
+								<Panel.Body>Basic panel example</Panel.Body>
+							</Panel>
+						</Col>
+					<Col xs={6} md={4}>
+					</Col>
+					</Row>
+				</Grid>
+			</div>
 		)
 	}
 }
--- completed/src/index.js
+++ completed/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import TodoContainer from './TodoContainer'
 import registerServiceWorker from './registerServiceWorker'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<TodoContainer />, document.getElementById('root'))
 registerServiceWorker()
